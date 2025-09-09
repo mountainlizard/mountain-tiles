9a10573f0bb99ff986125a3cf2a1736bb5d8f304
trepidacious <trepidacious@gmail.com>
2025-09-09 14:39:04 +0100

    Improve comments

--- src/app/init.rs
+++ src/app/init.rs
@@ -39,9 +39,9 @@ impl App {
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
         egui_extras::install_image_loaders(&cc.egui_ctx);
 
-        // We also install our own image loaders - this replaces the `image`
-        // loader feature in egui_extras, so we can use additional parts
-        // of the url to handle transparent color setting etc.
+        // We also install our own image loaders - this works similarly to the `image`
+        // loader feature in egui_extras, but uses a `tileset://` protocol and includes
+        // a `TilesetMode` allowing us to handle transparent color setting etc.
         egui_utils::install_image_loaders(&cc.egui_ctx);
 
         // Restore data from eframe storage
